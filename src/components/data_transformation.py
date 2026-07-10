dd31f2fdf4bdffa2da186a5cc0803551623e2666
Arghya Chakraborty <arghya0812@gmail.com>
2026-07-11 01:17:17 +0530

    removed catboost & xgboost

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -59,7 +59,7 @@ class DataTransformation:
             )
             return preprocessor
         except Exception as e:
-            CustomException(e, sys)
+            raise CustomException(e, sys)
 
     def initiate_data_transformation(self, train_path, test_path):
         try:
@@ -74,17 +74,10 @@ class DataTransformation:
             target_column_name = "math_score"
             numerical_columns = ["writing_score", "reading_score"]
 
-            input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
+            input_feature_train_df = train_df.drop(columns=[target_column_name])
             target_feature_train_df = train_df[target_column_name]
 
-            input_feature_test_df = test_df.drop(columns=[target_column_name], axis=1)
-            target_feature_test_df = test_df[target_column_name]
-
-        
-            input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
-            target_feature_train_df = train_df[target_column_name]
-
-            input_feature_test_df = test_df.drop(columns=[target_column_name], axis=1)
+            input_feature_test_df = test_df.drop(columns=[target_column_name])
             target_feature_test_df = test_df[target_column_name]
 
             logging.info(
